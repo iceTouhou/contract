8b590cefc96e4f40cbe68705671bc6440e76f894
Remi_IO <i@linux.dog>
2021-10-16 04:51:14 +0800

    Update TouhouNFT.sol

--- TouhouNFT.sol
+++ TouhouNFT.sol
@@ -32,6 +32,20 @@ library Counters {
     }
 }
 
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+
+
 
 pragma solidity ^0.8.0;
 
@@ -79,8 +93,10 @@ contract TouhouNFT is ERC721, AccessControl  {
         }
     }
 
-
-    constructor() ERC721("TouhouNFT", "TOHO") {}
+    constructor() ERC721("TouhouNFT", "TOHO") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+    }
 
     function mint(address player, string memory URI)
         external onlyRole(MINTER_ROLE)
