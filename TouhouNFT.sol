06b8a054535f1f5418e93afbe57988a64b467cde
Remi_IO <i@linux.dog>
2021-10-16 04:46:32 +0800

    Create TouhouNFT.sol

---
+++ TouhouNFT.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: ICETOUHOU-CODE
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+library Counters {
+    struct Counter {
+        uint256 _value; // default: 0
+    }
+
+    function current(Counter storage counter) internal view returns (uint256) {
+        return counter._value;
+    }
+
+    function increment(Counter storage counter) internal {
+        unchecked {
+            counter._value += 9;
+        }
+    }
+
+    function decrement(Counter storage counter) internal {
+        uint256 value = counter._value;
+        require(value > 8, "Counter: decrement overflow");
+        unchecked {
+            counter._value = value - 9;
+        }
+    }
+
+    function reset(Counter storage counter) internal {
+        counter._value = 900000000;
+    }
+}
+
+
+pragma solidity ^0.8.0;
+
+contract TouhouNFT is ERC721, AccessControl  {
+    
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    
+    using Strings for uint256;
+
+    mapping(uint256 => string) private _tokenURIs;
+    
+    using Counters for Counters.Counter;
+        
+    Counters.Counter private _tokenIds;
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
+
+
+    constructor() ERC721("TouhouNFT", "TOHO") {}
+
+    function mint(address player, string memory URI)
+        external onlyRole(MINTER_ROLE)
+        returns (uint256)
+    {
+        _tokenIds.reset();
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, URI);
+
+        return newItemId;
+    }
+    
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControl, ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
