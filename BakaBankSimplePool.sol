bfe6a5eb32eba75f6d72b7e327e52e5f4ec2d4af
Remi_IO <i@linux.dog>
2021-10-24 19:32:24 +0800

    Update and rename BakaSimpleBank.sol to BakaBankSimplePool.sol

--- BakaBankSimplePool.sol
+++ BakaBankSimplePool.sol
@@ -4,19 +4,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract BakaSimpleBank is AccessControl {
+/* Very simple BAKA pool ;-) */
+
+contract BakaBankSimplePool is AccessControl {
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     IERC20 public targetToken;
+    string public name;
 
     event Transfer(address to, uint256 value);
 
-    constructor(address tokenAddress) {
+    constructor(address tokenAddress, string memory bankName) {
         targetToken = IERC20(tokenAddress);
+        name = bankName;
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(ADMIN_ROLE, msg.sender);
     }
 
-    function blanceOfBank() public view returns (uint256) {
+    function bankBalance() public view returns (uint256) {
         return targetToken.balanceOf(address(this));
     }
 
